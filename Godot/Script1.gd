630ae8bd65034c5f35cad36df4677e4046383e6b
horityfoxy <122602325+horityfoxy@users.noreply.github.com>
2023-02-04 20:16:09 +0300

    Create Script1.gd

---
+++ Godot/Script1.gd
@@ -0,0 +1,8 @@
+```# Закрытие игры при нажатии на ESC. В настройках проекта в "ui_cancel" прописана клавиша ESC.
+
+extends Node3D
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit() # Quits the game
+```
