e349be99859ccce3498e99fb27d3f265a5df8a30
horityfoxy <122602325+horityfoxy@users.noreply.github.com>
2023-02-04 20:16:28 +0300

    Update Script1.gd

--- Godot/Script1.gd
+++ Godot/Script1.gd
@@ -1,8 +1,7 @@
-```# Закрытие игры при нажатии на ESC. В настройках проекта в "ui_cancel" прописана клавиша ESC.
+# Закрытие игры при нажатии на ESC. В настройках проекта в "ui_cancel" прописана клавиша ESC.
 
 extends Node3D
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit() # Quits the game
-```
